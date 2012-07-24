8dc0be9059ee904f086e880cfc84a6496fa0a307
SimiTestetGit <simeon.wiedenmann@web.de>
2012-07-24 12:51:04 +0200

    Processing backup nach Gespräch mit Herrn Dr. Heinitz
    es stehen größere Veränderungen an, wie z.B. die Filterkoeffizienten in der Main Funktion zu berechnen und die Fractional Multiplikationsfunktion zu nutzen! Daher ist ein Backup sinnvoll!

--- Processing.c
+++ Processing.c
@@ -29,18 +29,20 @@ extern fract16        fOut_4;
 extern int iTxBuffer[];         // SPORT0 DMA transmit buffer
 extern int iRxBuffer[];         // SPORT0 DMA receive buffer
 //-------------------------------
-extern fract16		fBuf[4097];	//Buffer Array, letzter index ist trash
-extern fract16		fKof[4096];	//Koeffizienten Array
-extern int 		ic=0;		//Buffer-Load Variable
-extern fract16		fAcc;		//Hall Akkumulator für die Rückkopplung
-extern int 		iKoAnz;		// Anzahl an verwendeten Koeffizienten
+fract16		fBuf[4097];	//Buffer Array, letzter index ist trash
+fract16		fKof[4096];	//Koeffizienten Array
+int 		ic=0;		//Buffer-Load Variable
+fract16		fAcc;		//Hall Akkumulator für die Rückkopplung
+int 		iKoAnz;		// Anzahl an verwendeten Koeffizienten
 
 
 //-------------------------------	//Skalierungsfaktoren
-extern fract16		fInp_1Scale = 0.5;	//Skalierungsfaktor Eingang (fInp_1Scale+fAccScale =1)
-extern fract16		fAccScale = 0.5;	//Skalierter Ausgang
+ float		flInp_1Scale = 0.5;	//Skalierungsfaktor Eingang (fInp_1Scale+fAccScale =1)
+ float		flAccScale = 0.2;	//Skalierter Ausgang
 
-extern fract16		fKoeffAnz = 1/iKoAnz;	//bei iKoAnz (29) Koeffizienten	
+ float		flKoeffAnz = 0.03448275862068965517;
+ float        tmp;
+//extern fract16		float_to_fr16(flKoeffAnz) =1/iKoAnz;	//bei iKoAnz (29) Koeffizienten	
 
 //---------------------------------------------------------------------------
 // Prototypes                                                              //
@@ -91,46 +93,50 @@ void Process_B(void)
 		i++;	
 	}
 	
-	fBuf[0]=fInp_1*fInp_1Scale;			//neuen Wert aus ADC laden * skalierungsfaktor
- 	fBuf[0]+=fAcc*fAccScale;			// Rückkoplung vom Ausgang, skaliert
+	tmp=fr16_to_float(fInp_1);
+	fBuf[0]=mult_fr1x16(fInp_1,float_to_fr16(flInp_1Scale));;//*float_to_fr16(flInp_1Scale);			//neuen Wert aus ADC laden * skalierungsfaktor
+ 	//fBuf[0]+=fAcc*float_to_fr16(flAccScale);			// Rückkoplung vom Ausgang, skaliert
  	if(ic<4095){			//c max 4095
 		ic++;				//Buffer load erhöhen, falls nicht schon voll
 	}	
 	
 	// Koeffizienten berechnen
-	if(c>4094){
+	if(ic>4094){
 		fAcc=0;
-		fAcc+=fBuf[0]*fKoeffAnz;
-		fAcc+=fBuf[500]*float_to_frac16(exp(-500))*fKoeffAnz;
-		fAcc+=fBuf[1000]*float_to_frac16(exp(-1000))*fKoeffAnz;
-		fAcc+=fBuf[1500]*float_to_frac16(exp(-1500))*fKoeffAnz;
-		fAcc+=fBuf[2000]*float_to_frac16(exp(-2000))*fKoeffAnz;
-		fAcc+=fBuf[2500]*float_to_frac16(exp(-2500))*fKoeffAnz;
-		fAcc+=fBuf[3000]*float_to_frac16(exp(-3000))*fKoeffAnz;
-		fAcc+=fBuf[3050]*float_to_frac16(exp(-3050))*fKoeffAnz;
-		fAcc+=fBuf[3100]*float_to_frac16(exp(-3100))*fKoeffAnz;
-		fAcc+=fBuf[3150]*float_to_frac16(exp(-3150))*fKoeffAnz;
-		fAcc+=fBuf[3200]*float_to_frac16(exp(-3200))*fKoeffAnz;
-		fAcc+=fBuf[3250]*float_to_frac16(exp(-3250))*fKoeffAnz;
-		fAcc+=fBuf[3300]*float_to_frac16(exp(-3300))*fKoeffAnz;
-		fAcc+=fBuf[3350]*float_to_frac16(exp(-3350))*fKoeffAnz;
-		fAcc+=fBuf[3400]*float_to_frac16(exp(-3400))*fKoeffAnz;
-		fAcc+=fBuf[3450]*float_to_frac16(exp(-3450))*fKoeffAnz;
-		fAcc+=fBuf[3500]*float_to_frac16(exp(-3500))*fKoeffAnz;
-		fAcc+=fBuf[3550]*float_to_frac16(exp(-3550))*fKoeffAnz;
-		fAcc+=fBuf[3600]*float_to_frac16(exp(-3600))*fKoeffAnz;
-		fAcc+=fBuf[3650]*float_to_frac16(exp(-3650))*fKoeffAnz;
-		fAcc+=fBuf[3700]*float_to_frac16(exp(-3700))*fKoeffAnz;
-		fAcc+=fBuf[3750]*float_to_frac16(exp(-3750))*fKoeffAnz;
-		fAcc+=fBuf[3800]*float_to_frac16(exp(-3800))*fKoeffAnz;
-		fAcc+=fBuf[3850]*float_to_frac16(exp(-3850))*fKoeffAnz;
-		fAcc+=fBuf[3900]*float_to_frac16(exp(-3900))*fKoeffAnz;
-		fAcc+=fBuf[3950]*float_to_frac16(exp(-3950))*fKoeffAnz;
-		fAcc+=fBuf[4000]*float_to_frac16(exp(-4000))*fKoeffAnz;
-		fAcc+=fBuf[4050]*float_to_frac16(exp(-4050))*fKoeffAnz;
-		fAcc+=fBuf[4095]*float_to_frac16(exp(-4095))*fKoeffAnz;
+		fAcc+=fBuf[4000]*float_to_fr16(0.2);
+		//fAcc+=fBuf[0]*float_to_fr16(flKoeffAnz);
+		/*fAcc+=fBuf[500]*float_to_fr16(exp(-500))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[1000]*float_to_fr16(exp(-1000))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[1500]*float_to_fr16(exp(-1500))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[2000]*float_to_fr16(exp(-2000))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[2500]*float_to_fr16(exp(-2500))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3000]*float_to_fr16(exp(-3000))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3050]*float_to_fr16(exp(-3050))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3100]*float_to_fr16(exp(-3100))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3150]*float_to_fr16(exp(-3150))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3200]*float_to_fr16(exp(-3200))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3250]*float_to_fr16(exp(-3250))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3300]*float_to_fr16(exp(-3300))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3350]*float_to_fr16(exp(-3350))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3400]*float_to_fr16(exp(-3400))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3450]*float_to_fr16(exp(-3450))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3500]*float_to_fr16(exp(-3500))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3550]*float_to_fr16(exp(-3550))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3600]*float_to_fr16(exp(-3600))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3650]*float_to_fr16(exp(-3650))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3700]*float_to_fr16(exp(-3700))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3750]*float_to_fr16(exp(-3750))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3800]*float_to_fr16(exp(-3800))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3850]*float_to_fr16(exp(-3850))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3900]*float_to_fr16(exp(-3900))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[3950]*float_to_fr16(exp(-3950))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[4000]*float_to_fr16(exp(-4000))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[4050]*float_to_fr16(exp(-4050))*float_to_fr16(flKoeffAnz);
+		fAcc+=fBuf[4095]*float_to_fr16(exp(-4095))*float_to_fr16(flKoeffAnz);*/
 	}
-	fOut_1=fAcc;
+	fOut_1=fBuf[0];
+	PutDAC(DAC_1R, fOut_1);
+	PutDAC(DAC_1L, fOut_1);
  }
 //--------------------------------------------------------------------------//
 
